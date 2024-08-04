38e79cfbfb23abdf83f6b06ab867aa9aaaef7f55
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-08-04 13:53:08 +1000

    Visitor class

--- AhmedDEWAN-A2/src/Visitor.java
+++ AhmedDEWAN-A2/src/Visitor.java
@@ -1,2 +1,31 @@
 public class Visitor {
+    private String ticket;
+    private int visits;
+
+    public Visitor(){
+        super();
+        this.ticket = " ";      //Ticket Number
+        this.visits = 0;        //How many times Visitor has visited, for Reward purpose.
+    }
+
+    public Visitor (String name, int age, String suburb, String ticket, int visits){
+        super();
+        this.ticket = ticket;
+        this.visits = visits;
+    }
+
+ //getters and setters
+    public String getTicket(){
+        return ticket;
+    }
+    public void setTicket(String ticket){
+        this.ticket = ticket;
+    }
+    public int getVisits(){
+        return visits;
+    }
+    public void setVisits(int visits){
+        this.visits = visits;
+    }
+
 }
