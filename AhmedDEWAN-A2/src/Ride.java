7f0634a7c87662e08d57d59512cb46f74789e980
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-08-04 14:10:37 +1000

    half Ride class

--- AhmedDEWAN-A2/src/Ride.java
+++ AhmedDEWAN-A2/src/Ride.java
@@ -1,2 +1,22 @@
 public class Ride {
+    private String rideName;
+    private String type;
+    private Employee operator; //Referance from employee class
+
+    public Ride (){
+        this.rideName = " ";
+        this.type = "";
+        this.operator = null;
+    }
+
+    public Ride (String rideName, String type, Employee operator){
+        this.rideName = rideName;
+        this.type = type;
+        this.operator = operator;
+    }
+
+//Getters and setters
+    public String getRideName(){
+        return rideName;
+    }
 }
