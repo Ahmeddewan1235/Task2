a0b50749aa530c1e0fb7e525e85d5cb8653dd015
Ahmeddewan1235 <a.dewan.10@student.scu.edu.au>
2024-08-07 16:22:18 +1000

    Comparator

---
+++ AhmedDEWAN-A2/src/Comparator.java
@@ -0,0 +1,10 @@
+
+import java.util.Comparator;
+
+public class VisitorComparator implements Comparator<Visitor> {
+
+    @Override
+    public int compare(Visitor visitor1, Visitor visitor3){
+
+    }
+}
